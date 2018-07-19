8e4b15fdbc51bbffed57793c5313065b9212495b
Drew Diamantoukos <MercifulCode@gmail.com>
2018-07-19 15:32:30 -0400

    Prepublish hook

---
+++ lib/index.d.ts
@@ -0,0 +1,5 @@
+import { MatchMap } from './MatchMap';
+import { MatchResult } from './MatchResult';
+import * as nt from './nt';
+import { Seq } from './Seq';
+export { MatchMap, MatchResult, nt, Seq };
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import { MatchMap } from './MatchMap';
+import { MatchResult } from './MatchResult';
+import * as nt from './nt';
+import { Seq } from './Seq';
+
+export { MatchMap, MatchResult, nt, Seq };
